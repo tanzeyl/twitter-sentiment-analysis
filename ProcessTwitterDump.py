e32576f1941d5d2b539eee30ac000d85280d6326
Tanzeel Khan <ktanzeel80@gmail.com>
2021-08-25 17:19:43 +0530

    Adding geo locator

--- ProcessTwitterDump.py
+++ ProcessTwitterDump.py
@@ -4,10 +4,12 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tag import pos_tag
+from geopy.exc import GeocoderTimedOut
+from geopy.geocoders import Nominatim
 
 
 class MoodClassifier:
-    def __init__(self, classifier_file='my_classifier.pickle'):
+    def __init__(self, classifier_file='model.pickle'):
         f = open(classifier_file, 'rb')
         self.classifier = pickle.load(f)
         f.close()
@@ -48,6 +50,24 @@ class MoodClassifier:
         return category
 
 
+class Locator:
+    def __init__(self):
+        self.geo_locator = Nominatim(user_agent="LearnPython")
+        self.location_store = {}
+        self.lookups = 0
+
+    def get_location(self, location_name):
+        if location_name in self.location_store:
+            return self.location_store[location_name]
+        try:
+            self.lookups += 1
+            location = self.geo_locator.geocode(location_name, language = "en")
+            self.location_store[location_name] = location
+        except GeocoderTimedOut:
+            location = None
+        return location
+
+
 def process(input_file, output_file):
     tweets = None
     with open(input_file) as f:
@@ -55,7 +75,29 @@ def process(input_file, output_file):
 
     print("Number of tweets", len(tweets))
 
+    classifier = MoodClassifier()
+    locator = Locator()
+    cnt = 0
+    csv_data = []
+    for tweet in tweets:
+        csv_data_item = {"mood" : None, "location" : None, "latitude" : None, "longitude" : None}
+        if "retweeted_status" in tweet:
+            tweet = tweet["retweeted_status"]
+        csv_data_item["mood"] = classifier.get_mood(tweet["full_text"])
+        if "location" in tweet["user"]:
+            location = locator.get_location(tweet["user"]["location"])
+            if location:
+                csv_data_item["location"] = str(location.address).split(", ")[-1]
+                csv_data_item["latitude"] = location.latitude
+                csv_data_item["longitude"] = location.longitude
+        csv_data.append(csv_data_item)
+        cnt += 1
+        if cnt > 10:
+            break
+    print(csv_data)
+    # The percentage of positive mood is 0.5525186647215309. (Python)
+
 
 if __name__ == "__main__":
-    input_file = "tweets_with_python.json"
+    input_file = "tweets_with_java.json"
     process(input_file, None)
